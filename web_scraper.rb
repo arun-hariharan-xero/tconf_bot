916f605c85643481a3d4231e5a87f14de726171c
Arun Sivasubramaniyan <arun.sivasubramaniya@carsales.office>
2016-11-16 09:45:50 +1100

    checking basic faq method

--- web_scraper.rb
+++ web_scraper.rb
@@ -5,18 +5,21 @@ require 'httparty'
 require 'sinatra'
 
 post '/gateway' do
-  all_speakers_list = ["speakers", "list", "of", "all"]
+  all_speakers_list = ["speakers", "all"]
+  faq_init = ["faq"] 
+
   message = params[:text].gsub(params[:trigger_word], '').strip
   
-  if (message.split(' ') & all_speakers_list).any?
+  if (message.split(' ') & faq_init).any?
+    answer = fetch_faq_answer(message)
+    respond_message answer
+  elsif (message.split(' ') & all_speakers_list).any?
     respond_message get_speaker_hash
+  if (message.split(' ') & schedule_list).any? && (message.split(' ') & all_speakers_list).any?)
+
   else
     respond_message "Oops - you just asked a query that is being cooked into the bot-heart. Bad luck Brian!"
   end
-
-
-  # This is an array - not needed for now
-  # speaker_data = speakers.map.with_index { |e, i | e +", "+ speakers_title[i] }
 
   #Pry.start(binding)  
 end
@@ -24,6 +27,13 @@ end
 def respond_message message
     content_type :json
     {:text => message}.to_json
+end
+
+def fetch_faq_answer(message)
+  faq_list = ["bathroom", "talks", "location"]
+  if message.include? faq_list[0]
+    "The bathrooms can be found only in time of urgency. "
+  end
 end
 
 def get_speaker_hash

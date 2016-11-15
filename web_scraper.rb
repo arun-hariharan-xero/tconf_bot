b748cc0a5e912e04eb8b126813ec2f1168a57e36
Arun <arunshariharan@gmail.com>
2016-11-16 01:28:01 +1100

    Refactored code

--- web_scraper.rb
+++ web_scraper.rb
@@ -6,9 +6,28 @@ require 'httparty'
 require 'sinatra'
 
 post '/gateway' do
+  all_speakers_list = ["speakers", "list", "of", "all"]
   message = params[:text].gsub(params[:trigger_word], '').strip
 
-  all_speakers_list = ["speakers", "list", "of", "all"]
+  if (message.split(' ') & all_speakers_list).any?
+    respond_message get_speaker_hash
+  else
+    respond_message "Oops - you just asked a query that is being cooked into the bot-heart. Bad luck Brian!"
+  end
+
+
+  # This is an array - not needed for now
+  # speaker_data = speakers.map.with_index { |e, i | e +", "+ speakers_title[i] }
+
+  #Pry.start(binding)  
+end
+
+def respond_message message
+    content_type :json
+    {:text => message}.to_json
+end
+
+def get_speaker_hash
 
   page = HTTParty.get('https://tconf.io', verify: false)
   parse_page = Nokogiri::HTML(page)
@@ -27,22 +46,4 @@ post '/gateway' do
 
   # Map speakers with their title and company as a hash
   speaker_hash = Hash[speakers.zip(speakers_title)]
-
-
-  if (message.split(' ') & all_speakers_list).any?
-    respond_message speaker_hash
-  else
-    respond_message "Oops - you just asked a query that is being cooked into the bot-heart. Bad luck Brian!"
-  end
-
-
-  # This is an array - not needed for now
-  # speaker_data = speakers.map.with_index { |e, i | e +", "+ speakers_title[i] }
-
-  #Pry.start(binding)  
-end
-
-def respond_message message
-    content_type :json
-    {:text => message}.to_json
 end

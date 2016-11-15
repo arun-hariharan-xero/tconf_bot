a7d6e5d6ca2d6de1e12a30bdfcca7dc1cfc8e814
Arun <arunshariharan@gmail.com>
2016-11-16 01:07:21 +1100

    Configured to show speakers list in hash

--- web_scraper.rb
+++ web_scraper.rb
@@ -4,26 +4,43 @@ require 'pry'
 require 'csv'
 require 'httparty'
 
-page = HTTParty.get('https://tconf.io', verify: false)
-parse_page = Nokogiri::HTML(page)
+post '/gateway' do
+  message = params[:text].gsub(params[:trigger_word], '').strip
 
-speakers, speakers_title = [], []
+  all_speakers_list = ["speakers", "list", "of", "all"]
 
-parse_page.css('.speaker').css('.name').map do |name|
-  speaker_name = name.text
-  speakers << speaker_name
+  page = HTTParty.get('https://tconf.io', verify: false)
+  parse_page = Nokogiri::HTML(page)
+
+  speakers, speakers_title = [], []
+
+  parse_page.css('.speaker').css('.name').map do |name|
+    speaker_name = name.text
+    speakers << speaker_name
+  end
+
+  parse_page.css('.speaker').css('.text-alt').map do |title|
+    speaker_title = title.text
+    speakers_title << speaker_title
+  end
+
+  # Map speakers with their title and company as a hash
+  speaker_hash = Hash[speakers.zip(speakers_title)]
+
+  if (message.split(' ') & all_speakers_list).any?
+    respond_message speaker_hash
+  else
+    respond_message "Oops - you just asked a query that is being cooked into the bot-heart. Bad luck Brian!"
+  end
+
+
+  # This is an array - not needed for now
+  # speaker_data = speakers.map.with_index { |e, i | e +", "+ speakers_title[i] }
+
+  #Pry.start(binding)  
 end
 
-parse_page.css('.speaker').css('.text-alt').map do |title|
-  speaker_title = title.text
-  speakers_title << speaker_title
+def respond_message message
+    content_type :json
+    {:text => message}.to_json
 end
-
-# Map speakers with their title and company as a hash
-speaker_hash = Hash[speakers.zip(speakers_title)]
-
-
-# This is an array - not needed for now
-# speaker_data = speakers.map.with_index { |e, i | e +", "+ speakers_title[i] }
-
-#Pry.start(binding)

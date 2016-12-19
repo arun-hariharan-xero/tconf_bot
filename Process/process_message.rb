09d2becf983994f8cb0b72114147416d88ac6867
Arun Sivasubramaniyan <arun.sivasubramaniya@carsales.office>
2016-12-19 13:57:08 +1100

    Created directories, made the initializer lean

---
+++ Process/process_message.rb
@@ -0,0 +1,58 @@
+class ProcessMessage
+
+  def self.process(message, brains)
+
+    if (message.split(' ') & brains.all_speakers_list).any?
+      brains.respond_message(brains.get_speaker_hash)
+
+    elsif (message.split(' ') & brains.faq_init).any?
+      answer = brains.fetch_faq_answer(message)
+      if answer == "speaker"
+        brains.begin_individual_response(message)
+      elsif answer == []
+         brains.bingo(message)
+      elsif answer == "test"
+        code1 = "My only test case - Guess the language ;) \n```describe '#please_run_i_beg_you' do \n\tcontext 'when bot is running' do\n\t\tit { is_expected.to be_running }\n\tend\nend```"
+        code2 = "I *swear* this is in my code. \nI guess *my master* likes to just *watch the world burn*!!\n```elsif answer == \"question\"\n\trespond_answer(question)\nend```"
+        code = [code1, code2]
+        brains.respond_normal(code.sample)
+      elsif answer == "question"
+        brains.respond_normal(brains.fetch_question)
+      elsif answer == "bathroom"
+        brains.respond_bathroom
+      else
+        brains.respond_message(answer)
+      end
+
+    elsif (message.split(' ') & brains.joke_init).any?
+      brains.respond_message(brains.fetch_jokes)
+
+    elsif (message.split(' ') & brains.compliment).any? 
+      brains.respond_normal(brains.fetch_compliment)
+
+    elsif (message.split(' ') & brains.love).any?
+      brains.respond_normal(brains.fetch_love)
+
+    elsif (message.split(' ') & brains.snap).any?
+      brains.respond_normal(brains.fetch_snap)
+
+    elsif (message.split(' ') & brains.lol).any?
+      brains.respond_normal(brains.fetch_lol)
+
+    elsif (message.split(' ') & brains.wishes).any?
+      brains.respond_normal(brains.fetch_wishes)
+
+    elsif (message.split(' ') & brains.individual_list_1).any? || (message.split(' ') & brains.individual_list).any?
+      brains.begin_individual_response(message)
+
+    elsif (message.split(' ') & brains.schedule_list).any?
+      brains.process_schedule_data
+      brains.respond_message(["The Schedule for the day:", brains.final_schedule.join("\n")])  
+
+    else
+      brains.bingo(message)
+    end
+
+  end
+
+end
--- web_scraper.rb
+++ web_scraper.rb
@@ -1,4 +1,5 @@
-require_relative 'bot_brains'
+require_relative 'Scraper/bot_brains'
+require_relative 'Process/process_message'
 
 require 'nokogiri'
 require 'json'
@@ -8,49 +9,8 @@ require 'sinatra'
 
 post '/gateway' do
   brains = BotBrains.new
-
+  puts params
   message = params[:text].gsub(params[:trigger_word], '').strip
-
-  if (message.split(' ') & brains.all_speakers_list).any?
-    brains.respond_message(brains.get_speaker_hash)
-  elsif (message.split(' ') & brains.faq_init).any?
-    answer = brains.fetch_faq_answer(message)
-    if answer == "speaker"
-      brains.begin_individual_response(message)
-    elsif answer == []
-       brains.bingo(message)
-    elsif answer == "test"
-      code1 = "My only test case - Guess the language ;) \n```describe '#please_run_i_beg_you' do \n\tcontext 'when bot is running' do\n\t\tit { is_expected.to be_running }\n\tend\nend```"
-      code2 = "I *swear* this is in my code. \nI guess *my master* likes to just *watch the world burn*!!\n```elsif answer == \"question\"\n\trespond_answer(question)\nend```"
-      code = [code1, code2]
-      brains.respond_normal(code.sample)
-    elsif answer == "question"
-      brains.respond_normal(fetch_question)
-    elsif answer == "bathroom"
-      brains.respond_bathroom
-    else
-      brains.respond_message(answer)
-    end
-  elsif (message.split(' ') & brains.joke_init).any?
-    brains.respond_message(fetch_jokes)
-  elsif (message.split(' ') & brains.compliment).any?
-    brains.respond_normal(fetch_compliment)
-  elsif (message.split(' ') & brains.love).any?
-    brains.respond_normal(fetch_love)
-  elsif (message.split(' ') & brains.snap).any?
-    brains.respond_normal(fetch_snap)
-  elsif (message.split(' ') & brains.lol).any?
-    brains.respond_normal(fetch_lol)
-  elsif (message.split(' ') & brains.wishes).any?
-    brains.respond_normal(fetch_wishes)
-  elsif (message.split(' ') & brains.individual_list_1).any? || (message.split(' ') & brains.individual_list).any?
-    brains.begin_individual_response(message)
-  elsif (message.split(' ') & brains.schedule_list).any?
-    brains.process_schedule_data
-    brains.respond_message(["The Schedule for the day:", brains.final_schedule.join("\n")])  
-  else
-    brains.bingo(message)
-  end
-
+  ProcessMessage.process(message, brains)
   #Pry.start(binding)  
 end

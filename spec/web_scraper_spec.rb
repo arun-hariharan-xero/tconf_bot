0c769f6f50f199a7203ebf1cab89cce1fd2583af
Arun Sivasubramaniyan <arun.sivasubramaniya@carsales.office>
2016-12-19 11:44:09 +1100

    Moved the class to its own file

--- spec/web_scraper_spec.rb
+++ spec/web_scraper_spec.rb
@@ -1,8 +1,9 @@
 require_relative "../web_scraper.rb"
+require_relative "../bot_brains.rb"
 
 describe 'Brains' do 
   context '#respond_bathroom' do 
-    let(:brains) { Brains.new }
+    let(:brains) { BotBrains.new }
     it 'returns bathroom json'  do
       brains.process_schedule_data
       name = brains.fetch_ind_data("tconf what is Harini speaking about?")

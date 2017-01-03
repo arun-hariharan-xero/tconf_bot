412b3d400e4eb2491c5782e45820721c574addfe
Arun Sivasubramaniyan <arun.sivasubramaniya@carsales.office>
2017-01-03 17:00:21 +1100

    Added a few tests for BotBrains class

--- spec/bot_brains_spec.rb
+++ spec/bot_brains_spec.rb
@@ -12,11 +12,46 @@ describe 'BotBrains' do
     @keywords = JSON.parse(keywords_json)
   end 
 
-  context '#fetch_love' do 
-    let(:brains) { BotBrains.new(@keywords) }
+  let(:brains) { BotBrains.new(@keywords) }
+  let(:brains_double) { instance_double('BotBrains') }
+  let(:message) {"one plus one"}
 
-    it 'processes the msessage correctly' do 
-      brains.bing_answer_for("HTC 10?")
+  context '#initialize' do 
+    it 'initializes with correct objects' do 
+      expect(brains.response).to_not be nil 
+      expect(brains.keywords).to equal @keywords
+    end
+  end
+
+  context '#bing_answer_for' do 
+    it 'calls get_answer_from_bing() private method' do
+      expect(brains).to receive(:get_answer_from_bing).with(message)
+      brains.bing_answer_for(message)
+    end
+  end
+
+  context '#get_speaker_hash' do 
+    it 'calls process_speaker_data() private method' do 
+      expect(brains).to receive(:process_speaker_data)
+      brains.get_speaker_hash
+    end
+
+    it 'returns an Array' do 
+      expect(brains.get_speaker_hash.class).to be Array
+    end
+  end
+
+  context '#process_schedules' do
+    it 'calls process_schedules() private method' do 
+      expect(brains).to receive(:process_each_schedule)
+      brains.process_schedules
+    end
+  end
+
+  context '#get_location_faq' do 
+    it 'calls process_faq_details() private method' do 
+      expect(brains).to receive(:process_faq_details).with(message)
+      brains.get_location_faq(message)
     end
   end
 end
